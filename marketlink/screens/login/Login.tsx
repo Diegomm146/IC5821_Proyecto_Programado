68defa5c5fe6966017b755969a87221611739435
Diegomm146 <dmm1462003@gmail.com>
2024-04-18 09:36:21 -0600

    react con vite

---
+++ marketlink/screens/login/Login.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Login() {
+    // nada
+    return <h1> Hello World </h1>;
+}
+
+export default Login;
---
+++ marketlink/src/App.tsx
@@ -0,0 +1,8 @@
+
+import Login from '../screens/login/Login'; // Assuming the file path is correct, make sure the necessary dependencies are installed.
+
+function App(){
+  return <div><Login></Login></div>;
+}
+
+export default App;
